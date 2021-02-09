0cbf15134548d3c6246914c818ba9d8caf8675b9
Diogo Rafael Soares <68117@PROWAY.TREINA>
2021-02-09 20:02:18 -0300

    tuplas

---
+++ fundamentos.ts
@@ -0,0 +1,46 @@
+let meuBoolean: boolean = true;
+//    alert(meuBoolean);
+
+    // o number é um ponto flutuante,
+    // hexa, octal e binario
+    let meuOctal: number = 0o13;
+    alert(meuOctal);
+
+    let meuBinario: number = 0b11;
+    alert (meuBinario);
+
+    let meuHexa: number = 0xAB13;
+    alert(meuHexa);
+
+    let texto: string = "olá Typescript";
+    // alert (texto);
+
+    let logradouro: string = "Rua 7 de Setembro";
+    let numero: string = "123B";
+    let cidade: string = "Blumenau";
+    let estado: string = "SC";
+    alert("Endereço: " + logradouro + ", " + numero + "."   + cidade + " - "+ estado)
+
+    //com interpolação de string
+    alert(`Endereço: ${logradouro}, ${numero}. ${cidade} - ${estado} `);
+
+    // tipo array em ts
+    let meuArray: number[] = [1,2,3,4];
+    alert(meuArray);
+
+    //pode ser feito com o <> irá ter o mesmo efeito, e com isso fica tipado como array
+    let meuArray2: Array<number> = [1,2,3,4];
+
+    // Operador com tuplas
+    //exemplo de tuplas sao os dicionario
+    /* palavra(string) -> significado(string)*/
+    let campeaoBrasileiro: [String, number] = ["Palmeiras", 2016];
+    let campeaoBrasileiro1: [String, number] = ["Corinthians", 2015];
+    let campeaoBrasileiro2: [String, number] = ["Cruzeiro", 2014];
+
+    alert(campeaoBrasileiro[0] + " - " + campeaoBrasileiro[1]);
+
+    let campeaoBrasileiro3: [string, number] = ["Palmeiras", 2016];
+    campeaoBrasileiro3[0] = "teste";
+
+    alert(campeaoBrasileiro3);
